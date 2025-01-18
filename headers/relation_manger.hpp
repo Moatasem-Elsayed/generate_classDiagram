66698252402cc5f3748c18dbfa4733a7012910e8
Moatasem El Sayed <eng.moatasem.8@gmail.com>
2025-01-18 11:06:40 +0100

    quick patch

--- headers/relation_manger.hpp
+++ headers/relation_manger.hpp
@@ -1,19 +1,19 @@
 #ifndef _RELATION_MANGER_H
 #define _RELATION_MANGER_H
-#include<file_control.hpp>
-#include<vector>
-class RelationManger : public FileControl
-{
+#include <cstdint>
+#include <file_control.hpp>
+#include <vector>
+class RelationManger : public FileControl {
 private:
-    std::vector<std::string> m_class_database;
+  std::vector<std::string> m_class_database;
 
 public:
-    RelationManger(std::string filename);
-    void create_relations();
-    void add_relation(std::vector<std::string>classesNames,uint8_t direction,uint8_t relationType,std::string label,std::vector<std::string>multiplicity);
-    ~RelationManger();
+  RelationManger(std::string filename);
+  void create_relations();
+  void add_relation(std::vector<std::string> classesNames, uint8_t direction,
+                    uint8_t relationType, std::string label,
+                    std::vector<std::string> multiplicity);
+  ~RelationManger();
 };
-
-
 
 #endif /*_RELATION_MANGER_H*/
